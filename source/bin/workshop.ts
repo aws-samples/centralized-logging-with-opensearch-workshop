a95d5a6fc13435736def4a030c9df120a47f8d21
Haidong <whaidong@amazon.com>
2021-12-09 15:19:59 +0800

    fix initial cdk test and build

--- source/bin/workshop.ts
+++ source/bin/workshop.ts
@@ -4,4 +4,4 @@ import 'source-map-support/register';
 import { MainStack } from '../lib/workshop-stack';
 
 const app = new cdk.App();
-new MainStack(app, 'CdkSolutionStack');
+new MainStack(app, 'CdkWorkshopStack');
