472c65d6939bdd2400e852d430e78e6cc628c75f
Haidong <whaidong@amazon.com>
2021-12-13 17:08:18 +0800

    complete build workshop website with steps: 1. create cloudfront, s3 and their log bucket. 2. upload assets to s3. 3. create rds mysql. 4. create alb, asg, ec2s. 5. when creating ec2, use usersdata to download php from s3, and run appache to host website
    cr: https://code.amazon.com/reviews/CR-61969299

--- source/lib/user-data.sh
+++ source/lib/user-data.sh
@@ -3,8 +3,14 @@
 sudo su
 yum update -y
 yum install -y httpd
+yum install -y mysql
+amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
 
 systemctl start httpd
 systemctl enable httpd
+
+mkdir -p /var/www/inc
+myValue=$(aws secretsmanager get-secret-value --secret-id logHubWorkshopSecret --query SecretString --output text --region us-east-1)
+echo "$myValue" > /var/www/inc/dbinfo.txt
 
 echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
--- source/lib/workshop-stack.ts
+++ source/lib/workshop-stack.ts
@@ -11,15 +11,34 @@
  *  and limitations under the License.
  */
 import {readFileSync} from 'fs';
- 
+import * as path from 'path'
+
 import * as au from '@aws-cdk/aws-autoscaling';
 import * as cdk from '@aws-cdk/core';
 import * as ec2 from '@aws-cdk/aws-ec2' // import ec2 library 
 import * as elbv2 from '@aws-cdk/aws-elasticloadbalancingv2' // import elb2 library
+import * as rds from '@aws-cdk/aws-rds';
+import * as iam from '@aws-cdk/aws-iam';
+import * as s3d from '@aws-cdk/aws-s3-deployment';
+import { CloudFrontToS3 } from '@aws-solutions-constructs/aws-cloudfront-s3';
+
+const workshopDB_user = 'logHubWorkshopUser';
+const workshopDB_secretName = 'logHubWorkshopSecret'
+const workshopDB_name = 'workshopDB';
 
 export class MainStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // S3 and cloudfront
+    const cloudFrontToS3 = new CloudFrontToS3(this, 'log-hub-workshop-cloudfront-s3', {});
+    const s3Bucket = cloudFrontToS3.s3Bucket!;
+    // upload simple web page and static file to s3
+    new s3d.BucketDeployment(this, 'DeployWebAssets', {
+      sources: [s3d.Source.asset(path.join(__dirname, '../s3'))],
+      destinationBucket: s3Bucket,
+      prune: false,
+    });
 
     // VPC
     const workshopVpc = new ec2.Vpc(this, 'workshopVpc', {
@@ -40,6 +59,41 @@ export class MainStack extends cdk.Stack {
       ]
     });
 
+    // RDS
+    // 1. create a secret manager first.
+    const rdsSecret = new rds.DatabaseSecret(
+      this,
+      'rdsSecret',
+      {
+          username: workshopDB_user,
+          secretName: workshopDB_secretName
+      }
+    );
+
+    // 2. security group
+    const dbSecurityGroup = new ec2.SecurityGroup(this, "workshopDBSecurityGroup", {
+      vpc: workshopVpc,
+      
+    })
+
+    // 3. create DB using the secret
+    const workshopDB = new rds.DatabaseInstance(this, 'workshopDB', {
+      engine: rds.DatabaseInstanceEngine.mysql({
+        version: rds.MysqlEngineVersion.VER_8_0_25
+      }),
+      vpc: workshopVpc,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PRIVATE_WITH_NAT
+      },
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.MICRO),
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      databaseName: workshopDB_name,
+      backupRetention: cdk.Duration.days(0),
+      credentials: rds.Credentials.fromSecret(rdsSecret, workshopDB_user),
+      publiclyAccessible: false,
+      securityGroups: [dbSecurityGroup]
+    });
+
     // ASG
     const workshopASG = new au.AutoScalingGroup(this, 'workshopASG', {
       instanceType: new ec2.InstanceType("t2.micro"),
@@ -51,10 +105,19 @@ export class MainStack extends cdk.Stack {
       vpcSubnets: {
         subnetType: ec2.SubnetType.PRIVATE_WITH_NAT,
         onePerAz: true
-      }
-    })
-
+      },
+      updatePolicy: au.UpdatePolicy.rollingUpdate()
+    });
+    workshopASG.node.addDependency(workshopDB);
+    workshopASG.role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore'));
+    workshopASG.role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('SecretsManagerReadWrite'));
+    workshopASG.role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonS3ReadOnlyAccess'));
     workshopASG.addUserData(readFileSync('./lib/user-data.sh', 'utf8'));
+    workshopASG.userData.addS3DownloadCommand({
+      bucket: s3Bucket,
+      bucketKey: "samplePage.php",
+      localFile: "/var/www/html/samplePage.php"
+    });
 
     // ELB
     const workshopAlb = new elbv2.ApplicationLoadBalancer(this, 'workshopAlb', {
@@ -67,14 +130,30 @@ export class MainStack extends cdk.Stack {
 
     const listener = workshopAlb.addListener('Listener', {
       port: 80
-    })
+    });
 
     listener.addTargets('ApplicationFleet', {
       port: 80,
       targets: [workshopASG]
-    })
+    });
+
+    // Connections
+    dbSecurityGroup.connections.allowFrom(workshopASG, ec2.Port.tcp(3306));
+    dbSecurityGroup.connections.allowFrom(workshopASG, ec2.Port.tcp(22));
     
+    // Outputs
     new cdk.CfnOutput(this, 'Region', { value: this.region })
     new cdk.CfnOutput(this, 'ALB CNAME', { value: workshopAlb.loadBalancerDnsName })
+    new cdk.CfnOutput(this, 'dbEndpoint', { value: workshopDB.instanceEndpoint.hostname });
+
+    new cdk.CfnOutput(this, 's3Bucket', {
+      value: cloudFrontToS3.s3Bucket?.bucketArn!,
+    });
+    new cdk.CfnOutput(this, 's3LoggingBucket', {
+      value: cloudFrontToS3.s3LoggingBucket?.bucketArn!,
+    });
+    new cdk.CfnOutput(this, 'cloudFrontLoggingBucket', {
+      value: cloudFrontToS3.cloudFrontLoggingBucket?.bucketArn!,
+    });
   }
 }
